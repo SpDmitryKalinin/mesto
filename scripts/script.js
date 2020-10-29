901cc14cbd18de95f4a747b1e15aea768cc53402
Dmitry Kalinin <spkalinin97@gmail.com>
2020-10-29 21:02:36 +0300

    Добавлен функционал валидации форм, выхода из модальных окон кликом по оверлею и нажатию кнопки esc

--- scripts/script.js
+++ scripts/script.js
@@ -44,7 +44,6 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-let counterCard = 0;
 
 //Инициализация массива
 initialCards.forEach(item =>{
